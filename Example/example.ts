b136bf697f364808be0ce1d72e6187145a3f0e4b
nazaltk <nazal.tk@gmail.com>
2021-11-17 22:40:39 +0530

    Added express

--- Example/example.ts
+++ Example/example.ts
@@ -11,6 +11,18 @@ import {
     waChatKey,
 } from '../src/WAConnection'
 import * as fs from 'fs'
+var express = require('express');
+var app     = express();
+
+app.set('port', (5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 async function example() {
     const conn = new WAConnection() // instantiate
